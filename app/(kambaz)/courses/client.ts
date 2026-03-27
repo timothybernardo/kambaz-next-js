08e7d450ab1914faa7c3de08654cbd8ace7ad676
timothybernardo <bernardo.t@northeastern.edu>
2026-03-26 20:16:27 -0400

    update client.ts

--- app/(kambaz)/courses/client.ts
+++ app/(kambaz)/courses/client.ts
@@ -41,6 +41,18 @@ export const updateModule = async (courseId: string, module: any) => {
   const { data } = await axiosWithCredentials.put(`${COURSES_API}/${courseId}/modules/${module._id}`, module);
   return data;
 };
+export const enrollIntoCourse = async (userId: string, courseId: string) => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/${userId}/courses/${courseId}`);
+  return response.data;
+};
+export const unenrollFromCourse = async (userId: string, courseId: string) => {
+  const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}/courses/${courseId}`);
+  return response.data;
+};
+export const findUsersForCourse = async (courseId: string) => {
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/users`);
+  return response.data;
+};
 export const findAssignmentsForCourse = async (courseId: string) => {
   const { data } = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/assignments`);
   return data;
