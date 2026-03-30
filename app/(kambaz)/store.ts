5edb0c92119939ef48868c92a95a1a3aa67f9d14
timothybernardo <bernardo.t@northeastern.edu>
2026-03-30 14:21:45 -0400

    a6 frontend updates

--- app/(kambaz)/store.ts
+++ app/(kambaz)/store.ts
@@ -4,6 +4,7 @@ import modulesReducer from "./courses/[cid]/modules/reducer";
 import accountReducer from "./account/reducer";
 import assignmentsReducer from "./courses/[cid]/assignments/reducer";
 import enrollmentsReducer from "./enrollments/reducer";
+import quizzesReducer from "./courses/[cid]/quizzes/reducer";
 
 const store = configureStore({
   reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
     accountReducer,
     assignmentsReducer,
     enrollmentsReducer,
+    quizzesReducer,
   },
 });
 
